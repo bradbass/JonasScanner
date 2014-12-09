86698e1134e521b4f7a08d209e64905b4c1ae063
brad.bass <brad.bass@af50db94-e364-4a19-a637-c7e1e1146647>
2014-12-09 17:07:39 +0000

    Removed unnecessary import.

--- BlueHarvest_TEST/src/com/jonasSoftware/blueharvest/TransferActivity.java
+++ BlueHarvest_TEST/src/com/jonasSoftware/blueharvest/TransferActivity.java
@@ -21,7 +21,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
